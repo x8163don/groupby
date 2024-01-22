60064b90ef742b7f2a1c1662789fb65b3464125c
Bevis <x8163don@gmail.com>
2024-01-22 11:57:18 +0800

    fix: eslint issue

--- src/component/SettingModal.js
+++ src/component/SettingModal.js
@@ -3,16 +3,10 @@ import {useEffect, useState} from "react";
 
 export default function SettingModal({isOpen, handleOpen, session, onGroupStrategyChange}) {
     const [curGroupStrategy, setCurGroupStrategy] = useState(session.groupStrategy?.constructor?.name)
-    console.log(session.groupStrategy?.constructor?.name);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {
-        setCurGroupStrategy(session.groupStrategy?.constructor?.name)
-    }, [])
 
     useEffect(() => {
         setCurGroupStrategy(session.groupStrategy?.constructor?.name)
-    }, [session])
+    }, [session.groupStrategy])
 
     const onGroupStrategyHandler = (e) => {
         onGroupStrategyChange(e.target.value);
@@ -22,8 +16,11 @@ export default function SettingModal({isOpen, handleOpen, session, onGroupStrate
         <DialogBody>
             <Typography>分組方式</Typography>
             <div className="flex flex-col">
-                <Radio name="group" value="PlayCountBaseGroup" defaultChecked={curGroupStrategy === "PlayCountBaseGroup"} onChange={onGroupStrategyHandler} label="依上場次數"/>
-                <Radio name="group" value="GenderBaseGroup" defaultChecked={curGroupStrategy === "GenderBaseGroup"} onChange={onGroupStrategyHandler} label="依照性別"/>
+                <Radio name="group" value="PlayCountBaseGroup"
+                       defaultChecked={curGroupStrategy === "PlayCountBaseGroup"} onChange={onGroupStrategyHandler}
+                       label="依上場次數"/>
+                <Radio name="group" value="GenderBaseGroup" defaultChecked={curGroupStrategy === "GenderBaseGroup"}
+                       onChange={onGroupStrategyHandler} label="依照性別"/>
             </div>
         </DialogBody>
     </Dialog>
