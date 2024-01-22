c955636c9d1a654d0da4958d43389b25832fda9c
Bevis <x8163don@gmail.com>
2024-01-22 12:20:43 +0800

    refactor: remove useless state

--- src/component/SettingModal.js
+++ src/component/SettingModal.js
@@ -1,16 +1,6 @@
 import {Dialog, DialogBody, Radio, Typography} from "@material-tailwind/react";
-import {useEffect, useState} from "react";
 
 export default function SettingModal({isOpen, handleOpen, session, onGroupStrategyChange}) {
-    const [curGroupStrategy, setCurGroupStrategy] = useState(session.groupStrategy?.constructor?.name)
-
-    // eslint-disable-next-line
-    useEffect(() => { setCurGroupStrategy(session.groupStrategy?.constructor?.name) }, [])
-
-    useEffect(() => {
-        setCurGroupStrategy(session.groupStrategy?.constructor?.name)
-    }, [session.groupStrategy])
-
     const onGroupStrategyHandler = (e) => {
         onGroupStrategyChange(e.target.value);
     }
@@ -19,11 +9,16 @@ export default function SettingModal({isOpen, handleOpen, session, onGroupStrate
         <DialogBody>
             <Typography>分組方式</Typography>
             <div className="flex flex-col">
-                <Radio name="group" value="PlayCountBaseGroup"
-                       defaultChecked={curGroupStrategy === "PlayCountBaseGroup"} onChange={onGroupStrategyHandler}
+                <Radio name="group"
+                       value="PlayCountBaseGroup"
+                       defaultChecked={session.groupStrategy.constructor.name === "PlayCountBaseGroup"}
+                       onChange={onGroupStrategyHandler}
                        label="依上場次數"/>
-                <Radio name="group" value="GenderBaseGroup" defaultChecked={curGroupStrategy === "GenderBaseGroup"}
-                       onChange={onGroupStrategyHandler} label="依照性別"/>
+                <Radio name="group"
+                       value="GenderBaseGroup"
+                       defaultChecked={session.groupStrategy.constructor.name === "GenderBaseGroup"}
+                       onChange={onGroupStrategyHandler}
+                       label="依照性別"/>
             </div>
         </DialogBody>
     </Dialog>
