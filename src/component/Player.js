69b89cc88293b59567732dd70830c31455c2dc46
Bevis <x8163don@gmail.com>
2024-01-29 18:40:11 +0800

    feat: makes reset status as black mask

--- src/component/Player.js
+++ src/component/Player.js
@@ -1,15 +1,19 @@
-import {Typography, Avatar} from "@material-tailwind/react";
+import {Typography} from "@material-tailwind/react";
 import Boy from "../assets/boy.svg";
 import Girl from "../assets/girl.svg";
 
 export default function Player({player, color, isShowStatus = false, isRest = false}) {
     return <div className="h-fit w-fit flex flex-col items-center">
-        <Avatar size="lg"
+        <div
+            className="relative max-w-xs overflow-hidden bg-cover bg-[50%] bg-no-repeat rounded-full">
+            <img
                 src={player.gender === "male" ? Boy : Girl}
-                withBorder={isShowStatus}
-                color={isRest ? "red" : "green"}
-                variant="circular"
-        />
+                className="h-12 w-12"/>
+            {
+                isRest && <div
+                    className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50"></div>
+            }
+        </div>
         <Typography variant="h6" color={color}>{player.name}</Typography>
     </div>
 }
